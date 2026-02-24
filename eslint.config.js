f6d21da0b591f9a51af7804d0d3585a57d876779
Pedro Santos <p3dro.santosc@gmail.com>
2026-02-24 10:55:27 -0300

    chore: add prettier setup

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from "@eslint/js"
 import globals from "globals"
 import tseslint from "typescript-eslint"
 import { defineConfig } from "eslint/config"
-
+import eslintConfigPrettier from "eslint-config-prettier"
 
 export default defineConfig([
   {
@@ -15,8 +15,5 @@ export default defineConfig([
     languageOptions: { globals: globals.node },
   },
   tseslint.configs.recommended,
-  ,
-
-
-
+  eslintConfigPrettier,
 ])
