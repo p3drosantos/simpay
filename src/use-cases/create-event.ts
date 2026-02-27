f6a3b9d8fb6270b43615f60593f3290a61a06919
Pedro Santos <p3dro.santosc@gmail.com>
2026-02-27 16:20:34 -0300

    fix: add correctly interface on CreateEventUseCase

--- src/use-cases/create-event.ts
+++ src/use-cases/create-event.ts
@@ -1,10 +1,11 @@
 import {
   createEventsParams,
+  ICreateEventsRepository,
   ICreateEventUseCase,
 } from "../controllers/events/protocols.js"
 
 export class CreateEventUseCase implements ICreateEventUseCase {
-  constructor(private createEventsRepository: ICreateEventUseCase) {}
+  constructor(private createEventsRepository: ICreateEventsRepository) {}
 
   async createEvent(params: createEventsParams) {
     return this.createEventsRepository.createEvent(params)
