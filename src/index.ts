15385e2a70236aeec096cec1ffa879641d37db0c
Pedro Santos <p3dro.santosc@gmail.com>
2026-02-27 15:52:59 -0300

    feat: add CreateEvent route

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
 import express from "express"
 import dotenv from "dotenv"
+import { CreateEventRepository } from "./repositories/create-event.js"
+import { CreateEventUseCase } from "./use-cases/create-event.js"
+import { CreateEventController } from "./controllers/events/create-event.js"
 dotenv.config()
 
 const app = express()
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!")
+app.post("/events", async (req, res) => {
+  try {
+    const createEventRepository = new CreateEventRepository()
+    const createEventUseCase = new CreateEventUseCase(createEventRepository)
+    const createEventController = new CreateEventController(createEventUseCase)
+
+    const response = await createEventController.createEvent({
+      body: req.body,
+    })
+
+    return res.status(response.statusCode).json(response.body)
+  } catch (error) {
+    console.error("ERRO NA ROTA /events:")
+    console.error(error)
+
+    return res.status(500).json({ error: String(error) })
+  }
 })
 
 app.listen(process.env.PORT, () => {
