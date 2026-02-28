fd37187b221e3c9650982a326ef1bc2019fb12a4
Pedro Santos <p3dro.santosc@gmail.com>
2026-02-27 23:20:53 -0300

    feat: add GetEventById route

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import dotenv from "dotenv"
 import { CreateEventRepository } from "./repositories/create-event.js"
 import { CreateEventUseCase } from "./use-cases/create-event.js"
 import { CreateEventController } from "./controllers/events/create-event.js"
+import { GetEventByIdRepository } from "./repositories/get-event-by-id.js"
+import { GetEventByIdUseCase } from "./use-cases/get-event-by-id.js"
+import { GetEventByIdController } from "./controllers/events/get-event-by-id.js"
 dotenv.config()
 
 const app = express()
@@ -21,6 +24,27 @@ app.post("/events", async (req, res) => {
     return res.status(response.statusCode).json(response.body)
   } catch (error) {
     console.error("ERRO NA ROTA /events:")
+    console.error(error)
+
+    return res.status(500).json({ error: String(error) })
+  }
+})
+
+app.get("/events/:id", async (req, res) => {
+  try {
+    const getEventByIdRepository = new GetEventByIdRepository()
+    const getEventByIdUseCase = new GetEventByIdUseCase(getEventByIdRepository)
+    const getEventByIdController = new GetEventByIdController(
+      getEventByIdUseCase
+    )
+
+    const response = await getEventByIdController.getEventById({
+      params: req.params,
+    })
+
+    return res.status(response.statusCode).json(response.body)
+  } catch (error) {
+    console.error("ERRO NA ROTA /events/:id:")
     console.error(error)
 
     return res.status(500).json({ error: String(error) })
