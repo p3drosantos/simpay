3e837be6e492e42dc859d9a8b0bfad686be5a767
Pedro Santos <p3dro.santosc@gmail.com>
2026-02-28 12:04:21 -0300

    feat: add GetAllEvents route

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import { CreateEventController } from "./controllers/events/create-event.js"
 import { GetEventByIdRepository } from "./repositories/get-event-by-id.js"
 import { GetEventByIdUseCase } from "./use-cases/get-event-by-id.js"
 import { GetEventByIdController } from "./controllers/events/get-event-by-id.js"
+import { GetAllEventsRepository } from "./repositories/get-all-events.js"
+import { GetAllEventsController } from "./controllers/events/get-all-events.js"
+import { GetAllEventsUseCase } from "./use-cases/get-all-events.js"
 dotenv.config()
 
 const app = express()
@@ -45,6 +48,24 @@ app.get("/events/:id", async (req, res) => {
     return res.status(response.statusCode).json(response.body)
   } catch (error) {
     console.error("ERRO NA ROTA /events/:id:")
+    console.error(error)
+
+    return res.status(500).json({ error: String(error) })
+  }
+})
+
+app.get("/events", async (req, res) => {
+  try {
+    const getAllEventsRepository = new GetAllEventsRepository()
+    const getAllEventsUseCase = new GetAllEventsUseCase(getAllEventsRepository)
+    const getAllEventsController = new GetAllEventsController(
+      getAllEventsUseCase
+    )
+
+    const response = await getAllEventsController.getAllEvents()
+    return res.status(response.statusCode).json(response.body)
+  } catch (error) {
+    console.error("ERRO NA ROTA /events:")
     console.error(error)
 
     return res.status(500).json({ error: String(error) })
