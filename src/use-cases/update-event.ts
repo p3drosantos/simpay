bbd8f74c16a87d7e4f37187c61b9433502d05135
Pedro Santos <p3dro.santosc@gmail.com>
2026-02-28 22:52:38 -0300

    feat: add UpdateEventUseCase

---
+++ src/use-cases/update-event.ts
@@ -0,0 +1,19 @@
+import {
+  IUpdateEventRepository,
+  IUpdateEventUseCase,
+} from "../controllers/events/protocols.js"
+import { UpdateEventInput } from "../validators/create-event.schema.js"
+import { Event } from "../models/event.js"
+
+export class UpdateEventUseCase implements IUpdateEventUseCase {
+  constructor(private updateEventRepository: IUpdateEventRepository) {}
+
+  async updateEvent(
+    id: string,
+    data: Partial<UpdateEventInput>
+  ): Promise<Event | null> {
+    const updatedEvent = await this.updateEventRepository.updateEvent(id, data)
+
+    return updatedEvent
+  }
+}
