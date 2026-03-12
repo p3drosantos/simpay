e7c480cf31a5e08f637096d57f0e288bc09d2ef8
Pedro Santos <p3dro.santosc@gmail.com>
2026-03-12 11:10:04 -0300

    test(event): add DeleteEventUseCase unit tests

---
+++ tests/unit/use-cases/delete-event.test.ts
@@ -0,0 +1,80 @@
+import { DeleteEventUseCase } from "../../../src/use-cases/events/delete-event"
+
+const makeSut = () => {
+  const mockDeleteEventRepository = {
+    deleteEvent: jest.fn(),
+  }
+
+  const mockGetEventByIdRepository = {
+    getEventById: jest.fn(),
+  }
+
+  const sut = new DeleteEventUseCase(
+    mockDeleteEventRepository,
+    mockGetEventByIdRepository
+  )
+
+  return {
+    sut,
+    mockDeleteEventRepository,
+    mockGetEventByIdRepository,
+  }
+}
+
+const mockEvent = {
+  id: "event-id",
+  ownerId: "user-id",
+  name: "Evento teste",
+  ticketPriceInCents: 1000,
+  longitude: 10,
+  latitude: 10,
+  date: new Date(),
+}
+
+describe("DeleteEventUseCase", () => {
+  it("should delete an event successfully", async () => {
+    const { sut, mockDeleteEventRepository, mockGetEventByIdRepository } =
+      makeSut()
+
+    mockGetEventByIdRepository.getEventById.mockResolvedValue(mockEvent)
+
+    mockDeleteEventRepository.deleteEvent.mockResolvedValue(mockEvent)
+
+    const response = await sut.deleteEvent("event-id", "user-id")
+
+    expect(response).toEqual(mockEvent)
+    expect(mockGetEventByIdRepository.getEventById).toHaveBeenCalledWith(
+      "event-id"
+    )
+    expect(mockDeleteEventRepository.deleteEvent).toHaveBeenCalledWith(
+      "event-id"
+    )
+  })
+  it("should throw EventNotFoundError if event does not exist", async () => {
+    const { sut, mockGetEventByIdRepository, mockDeleteEventRepository } =
+      makeSut()
+
+    mockGetEventByIdRepository.getEventById.mockResolvedValue(null)
+
+    await expect(sut.deleteEvent("event-id", "user-id")).rejects.toThrow(
+      "Event not found"
+    )
+
+    expect(mockDeleteEventRepository.deleteEvent).not.toHaveBeenCalled()
+  })
+
+  it("should throw UnauthorizedError if user is not the owner", async () => {
+    const { sut, mockGetEventByIdRepository, mockDeleteEventRepository } =
+      makeSut()
+
+    mockGetEventByIdRepository.getEventById.mockResolvedValue({
+      ...mockEvent,
+      ownerId: "another-user-id",
+    })
+
+    await expect(sut.deleteEvent("event-id", "user-id")).rejects.toThrow(
+      "Unauthorized"
+    )
+    expect(mockDeleteEventRepository.deleteEvent).not.toHaveBeenCalled()
+  })
+})
