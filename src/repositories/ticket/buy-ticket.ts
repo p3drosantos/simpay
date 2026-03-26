30ab4a9f7810d4d101f35ec7a4a6e06f9a84bd9f
Pedro Santos <p3dro.santosc@gmail.com>
2026-03-26 14:34:47 -0300

    feat: add updateTicket method

--- src/repositories/ticket/buy-ticket.ts
+++ src/repositories/ticket/buy-ticket.ts
@@ -5,7 +5,11 @@ import { eq, sql } from "drizzle-orm"
 
 import * as schema from "../../db/schema.js"
 import { Ticket } from "../../models/ticket.js"
-import { CreateTicketParams } from "../../types/ticket.js"
+import {
+  CreateTicketParams,
+  TicketStatus,
+  UpdateTicketData,
+} from "../../types/ticket.js"
 
 export class BuyTicketRepository implements IBuyTicketRepository {
   async buyTicket(params: CreateTicketParams): Promise<Ticket> {
@@ -16,6 +20,7 @@ export class BuyTicketRepository implements IBuyTicketRepository {
         buyerId: params.buyerId,
         quantity: params.quantity,
         totalPriceInCents: params.totalPriceInCents,
+        status: params.status,
       })
       .returning()
 
@@ -25,6 +30,7 @@ export class BuyTicketRepository implements IBuyTicketRepository {
       buyerId: ticket.buyerId,
       quantity: ticket.quantity,
       totalPriceInCents: ticket.totalPriceInCents,
+      status: ticket.status as TicketStatus,
       createdAt: new Date(ticket.createdAt),
     }
   }
@@ -40,5 +46,23 @@ export class BuyTicketRepository implements IBuyTicketRepository {
       .where(eq(schema.ticketsTable.eventId, eventId))
 
     return Number(result[0].total)
+  }
+
+  async updateTicket(ticketId: string, data: UpdateTicketData) {
+    const [ticket] = await db
+      .update(schema.ticketsTable)
+      .set(data)
+      .where(eq(schema.ticketsTable.id, ticketId))
+      .returning()
+
+    return {
+      id: ticket.id,
+      eventId: ticket.eventId,
+      buyerId: ticket.buyerId,
+      quantity: ticket.quantity,
+      totalPriceInCents: ticket.totalPriceInCents,
+      status: ticket.status as TicketStatus,
+      createdAt: new Date(ticket.createdAt),
+    }
   }
 }
