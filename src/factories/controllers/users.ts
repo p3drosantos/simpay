1ed11bac8d4e074a34b11165f965979405905d08
Pedro Santos <p3dro.santosc@gmail.com>
2026-03-29 13:16:40 -0300

    feat: add router GetUserTickets

--- src/factories/controllers/users.ts
+++ src/factories/controllers/users.ts
@@ -1,10 +1,13 @@
 import { BcryptAdapter } from "../../adapters/bcrypt-adapter.js"
 import { JwtAdapter } from "../../adapters/jwt-adapter.js"
 import { LoginController } from "../../controllers/auth/login.js"
+import { GetUserTicketsController } from "../../controllers/tickets/get-user-tickets.js"
 import { CreateUserController } from "../../controllers/users/create-user.js"
+import { GetUserTicketsRepository } from "../../repositories/ticket/get-user-tickets.js"
 import { CreateUserRepository } from "../../repositories/users/create-user.js"
 import { GetUserByEmailRepository } from "../../repositories/users/get-user-by-email.js"
 import { LoginUseCase } from "../../use-cases/auth/login.js"
+import { GetUserTicketsUseCase } from "../../use-cases/ticket/get-user-tickets.js"
 import { CreateUserUseCase } from "../../use-cases/users/create-user.js"
 
 export const makeCreateUserController = () => {
@@ -33,4 +36,15 @@ export const makeLoginController = () => {
   const loginController = new LoginController(loginUseCase)
 
   return loginController
+}
+
+export const makeGetUserTicketsController = () => {
+  const getUserTicketsRepository = new GetUserTicketsRepository()
+  const getUserTicketsUseCase = new GetUserTicketsUseCase(
+    getUserTicketsRepository
+  )
+  const getUserTicketsController = new GetUserTicketsController(
+    getUserTicketsUseCase
+  )
+  return getUserTicketsController
 }
