a23bf8b94a6c28fa5bdd332888c6f4fa87cb02df
Pedro Santos <p3dro.santosc@gmail.com>
2026-03-29 17:28:57 -0300

    test(e2e): add GetUserTickets e2e tests

--- tests/e2e/users.e2e.test.ts
+++ tests/e2e/users.e2e.test.ts
@@ -9,6 +9,50 @@ describe("Users E2E", () => {
     role: "customer" as const,
   }
 
+  async function createUserAndLogin(
+    email = "pedro@test.com",
+    role: "customer" | "organizer" = "customer"
+  ) {
+    await request(app).post("/users").send({
+      name: "Pedro",
+      email,
+      password: "123456",
+      role,
+    })
+
+    const login = await request(app).post("/users/login").send({
+      email,
+      password: "123456",
+    })
+
+    return login.body.token
+  }
+
+  async function createEvent(token: string, date = "2099-01-01") {
+    const res = await request(app)
+      .post("/events")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        name: "Event",
+        maxTickets: 10,
+        ticketPriceInCents: 100,
+        longitude: 10,
+        latitude: 10,
+        date,
+      })
+
+    return res.body.id
+  }
+
+  async function buyTicket(token: string, eventId: string) {
+    await request(app)
+      .post(`/events/${eventId}/tickets`)
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        quantity: 1,
+      })
+  }
+
   it("should create user", async () => {
     const res = await request(app).post("/users").send(userBody)
 
@@ -62,5 +106,54 @@ describe("Users E2E", () => {
     })
 
     expect(res.status).toBe(404)
+  })
+
+  it("should return authenticated user's tickets", async () => {
+    const token = await createUserAndLogin()
+
+    const eventId = await createEvent(token)
+
+    await buyTicket(token, eventId)
+
+    const res = await request(app)
+      .get("/users/me/tickets")
+      .set("Authorization", `Bearer ${token}`)
+
+    expect(res.status).toBe(200)
+    expect(res.body.length).toBe(1)
+    expect(res.body[0].event.id).toBe(eventId)
+  })
+
+  it("should return 401 when user is not authenticated", async () => {
+    const res = await request(app).get("/users/me/tickets")
+
+    expect(res.status).toBe(401)
+  })
+
+  it("should return empty array when user has no tickets", async () => {
+    const token = await createUserAndLogin("empty@test.com")
+
+    const res = await request(app)
+      .get("/users/me/tickets")
+      .set("Authorization", `Bearer ${token}`)
+
+    expect(res.status).toBe(200)
+    expect(res.body).toEqual([])
+  })
+
+  it("should not return tickets from other users", async () => {
+    const tokenUser1 = await createUserAndLogin("user1@test.com")
+    const tokenUser2 = await createUserAndLogin("user2@test.com")
+
+    const eventId = await createEvent(tokenUser1)
+
+    await buyTicket(tokenUser1, eventId)
+
+    const res = await request(app)
+      .get("/users/me/tickets")
+      .set("Authorization", `Bearer ${tokenUser2}`)
+
+    expect(res.status).toBe(200)
+    expect(res.body).toEqual([])
   })
 })
